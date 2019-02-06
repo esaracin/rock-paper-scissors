85e61e655a721ab2d3567308aff3b0bfd8060f37
Eli Saracino <esaracin@bu.edu>
2019-02-06 12:54:48 -0500

    n_gram implementation

--- N_gram.py
+++ N_gram.py
@@ -15,10 +15,21 @@ class N_gram:
     def __init__(self, n):
         self.n = n
     
-    def getMostFrequent(self, prev_moves):
+    def get_most_frequent(self, prev_moves):
         num_moves = len(prev_moves)
-        if num_moves == 0:
-            return random.choice(['r', 'p', 's'])
 
-        return 0
+        # Get the last n-moves
+        last_moves = prev_moves[-self.n:]
+
+        if num_moves == []: # Error check for first case, throw choice randomly
+            return random.choice(['r', 'p', 's'])
+        else:
+            counts = {'r': 0, 'p': 0, 's': 0}
+            for move in last_moves:
+                counts[move] += 1
+
+            most_freq = max(counts.values())
+            options = [choice for choice in counts if counts[choice] == most_freq]
+
+            return random.choice(options)
 
